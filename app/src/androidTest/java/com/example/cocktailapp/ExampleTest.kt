f0d7b870a42b9f9d4188c59085874c5c6e5a3553
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-02-15 22:58:30 +0300

    add ExampleTest

---
+++ app/src/androidTest/java/com/example/cocktailapp/ExampleTest.kt
@@ -0,0 +1,17 @@
+package com.example.cocktailapp
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    // TODO: Add tests
+}
